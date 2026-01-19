599ea35443fbbeaabe0a74471c4e6fa7c78b22b5
HiteshVerma44 <Hitesh.verma@infinite.com>
2026-01-19 16:57:06 +0530

    without ssr

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,12 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+  .catch(err => console.error(err));
+
+
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { appConfig } from './app/app.config';
+// import { AppComponent } from './app/app.component';
+
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
